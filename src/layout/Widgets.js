65bbf1b9a10b412c4513647ed55539897cbbd116
selincaglarr <selincaglarscc@gmail.com>
2022-02-10 15:57:00 +0300

    searchBar add

--- src/layout/Widgets.js
+++ src/layout/Widgets.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+
+import { SearchIcon } from "../icons/Icon";
 
 const Widgets = () => {
   return (
-    <aside className="bg-green-200 w-80" >Widgets</aside>
-  )
-}
+    <aside className="w-80">
+    <div className="flex items-center space-x-4 p-3 m-3 bg-gray-200 rounded-full text-gray-dark focus-within:bg-white focus-within:ring-1 focus-within:ring-primary-base focus-within:text-primary-base">
+      <SearchIcon className="w-5 h-5" />
+      <input
+        type="text"
+        placeholder="Search Twitter"
+        className="placeholder-gray-dark bg-transparent focus:outline-none w-full text-sm"
+      />
+    </div>
+      <div className="mt-5">
 
-export default Widgets
+      </div>
+    </aside>
+  );
+};
+
+export default Widgets;
